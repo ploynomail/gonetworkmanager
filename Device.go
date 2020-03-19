ac6a5d93daa1063ac95075f1b4ad0c66c6c22cd5
Josef Filzmaier <j.filzmaier@gmx.at>
2020-03-19 10:26:22 +0100

    Implement SetPropertyManaged
    For the Device interface

--- Device.go
+++ Device.go
@@ -124,6 +124,7 @@ type Device interface {
 
 	// Whether or not this device is managed by NetworkManager. Setting this property has a similar effect to configuring the device as unmanaged via the keyfile.unmanaged-devices setting in NetworkManager.conf. Changes to this value are not persistent and lost after NetworkManager restart.
 	GetPropertyManaged() (bool, error)
+	SetPropertyManaged(bool)
 
 	// If TRUE, indicates the device is allowed to autoconnect. If FALSE, manual intervention is required before the device will automatically connect to a known network, such as activating a connection using the device, or setting this property to TRUE. This property cannot be set to TRUE for default-unmanaged devices, since they never autoconnect.
 	GetPropertyAutoConnect() (bool, error)
@@ -256,6 +257,10 @@ func (d *device) GetPropertyDHCP6Config() (DHCP6Config, error) {
 
 func (d *device) GetPropertyManaged() (bool, error) {
 	return d.getBoolProperty(DevicePropertyManaged)
+}
+
+func (d *device) SetPropertyManaged(managed bool) error {
+	return d.setProperty(DevicePropertyManaged, managed)
 }
 
 func (d *device) GetPropertyAutoConnect() (bool, error) {
